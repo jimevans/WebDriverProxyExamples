e14c8e286dbb177372d75ead8507e003b6f1bf82
Jim Evans <james.h.evans.jr@gmail.com>
2013-08-09 10:52:18 -0400

    Initial commit

---
+++ ExtensionMethods.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiddler;
+using OpenQA.Selenium;
+
+namespace HttpStatusCodeExample
+{
+    static class ExtensionMethods
+    {
+        private static TimeSpan DefaultTimeout = TimeSpan.FromSeconds(10);
+
+        /// <summary>
+        /// Navigates to a specified URL, returning the HTTP status code of the navigation.
+        /// </summary>
+        /// <param name="driver">The driver used to navigate to the URL.</param>
+        /// <param name="targetUrl">The URL to navigate to.</param>
+        /// <returns>The HTTP status code of the navigation.</returns>
+        public static int NavigateTo(this IWebDriver driver, string targetUrl)
+        {
+            return NavigateTo(driver, targetUrl, DefaultTimeout);
+        }
+
+        /// <summary>
+        /// Navigates to a specified URL, returning the HTTP status code of the navigation.
+        /// </summary>
+        /// <param name="driver">The driver used to navigate to the URL.</param>
+        /// <param name="targetUrl">The URL to navigate to.</param>
+        /// <param name="timeout">A <see cref="TimeSpan"/> structure for the time out of the navigation.</param>
+        /// <returns>The HTTP status code of the navigation.</returns>
+        public static int NavigateTo(this IWebDriver driver, string targetUrl, TimeSpan timeout)
+        {
+            int responseCode = 0;
+            DateTime endTime = DateTime.Now.Add(timeout);
+            SessionStateHandler responseHandler = delegate(Session targetSession)
+            {
+                if (targetSession.fullUrl == targetUrl)
+                {
+                    responseCode = targetSession.responseCode;
+                }
+            };
+
+            FiddlerApplication.AfterSessionComplete += responseHandler;
+            driver.Url = targetUrl;
+            while (responseCode == 0 && DateTime.Now < endTime)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            FiddlerApplication.AfterSessionComplete -= responseHandler;
+            return responseCode;
+        }
+
+        /// <summary>
+        /// Clicks on a link that is expected to navigate to a new URL, returning
+        /// the HTTP status code of the navigation.
+        /// </summary>
+        /// <param name="element">The element clicked on to perform the navigation.</param>
+        /// <returns>The HTTP status code of the navigation.</returns>
+        public static int ClickNavigate(this IWebElement element)
+        {
+            return ClickNavigate(element, DefaultTimeout);
+        }
+
+        public static int ClickNavigate(this IWebElement element, TimeSpan timeout)
+        {
+            int responseCode = 0;
+            string urlNavigated = string.Empty;
+            SessionStateHandler responseHandler = delegate(Session targetSession)
+            {
+                // Get the response code of the first HTML response we get.
+                // This algorithm could be much more sophisticated based on your needs.
+                if (targetSession.oResponse["Content-Type"].Contains("text/html") && responseCode == 0)
+                {
+                    // If the response code is a redirect, ignore it so that we
+                    // get the "final" status code.
+                    if (targetSession.responseCode < 300 || targetSession.responseCode >= 400)
+                    {
+                        responseCode = targetSession.responseCode;
+                    }
+                }
+            };
+
+            FiddlerApplication.AfterSessionComplete += responseHandler;
+            DateTime endTime = DateTime.Now.Add(timeout);
+            element.Click();
+            while (responseCode == 0 && DateTime.Now < endTime)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            FiddlerApplication.AfterSessionComplete -= responseHandler;
+            return responseCode;
+        }
+    }
+}
---
+++ WebDriverFactory.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+
+namespace HttpStatusCodeExample
+{
+    static class WebDriverFactory
+    {
+        /// <summary>
+        /// Creates a WebDriver instance for the desired browser using the specified proxy settings.
+        /// </summary>
+        /// <param name="kind">The browser to launch.</param>
+        /// <param name="proxy">The WebDriver Proxy object containing the proxy settings.</param>
+        /// <returns>A WebDriver instance using the specified proxy settings.</returns>
+        public static IWebDriver CreateWebDriverWithProxy(BrowserKind kind, Proxy proxy)
+        {
+            IWebDriver driver = null;
+            switch (kind)
+            {
+                case BrowserKind.InternetExplorer:
+                    driver = CreateInternetExplorerDriverWithProxy(proxy);
+                    break;
+
+                case BrowserKind.Firefox:
+                    driver = CreateFirefoxDriverWithProxy(proxy);
+                    break;
+
+                case BrowserKind.Chrome:
+                    driver = CreateChromeDriverWithProxy(proxy);
+                    break;
+
+                default:
+                    driver = CreatePhantomJSDriverWithProxy(proxy);
+                    break;
+            }
+
+            return driver;
+        }
+
+        /// <summary>
+        /// Creates an InternetExplorerDriver instance using the specified proxy settings.
+        /// </summary>
+        /// <param name="proxy">The WebDriver Proxy object containing the proxy settings.</param>
+        /// <returns>An InternetExplorerDriver instance using the specified proxy settings</returns>
+        private static IWebDriver CreateInternetExplorerDriverWithProxy(Proxy proxy)
+        {
+            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+            ieOptions.Proxy = proxy;
+
+            // Make IE not use the system proxy, and clear its cache before
+            // launch. This makes the behavior of IE consistent with other
+            // browsers' behavior.
+            ieOptions.UsePerProcessProxy = true;
+            ieOptions.EnsureCleanSession = true;
+
+            IWebDriver driver = new InternetExplorerDriver(ieOptions);
+            return driver;
+        }
+
+        /// <summary>
+        /// Creates an FirefoxDriver instance using the specified proxy settings.
+        /// </summary>
+        /// <param name="proxy">The WebDriver Proxy object containing the proxy settings.</param>
+        /// <returns>An FirefoxDriver instance using the specified proxy settings</returns>
+        private static IWebDriver CreateFirefoxDriverWithProxy(Proxy proxy)
+        {
+            // A future version of the .NET Firefox driver will likely move
+            // to an "Options" model to be more consistent with other browsers'
+            // API.
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.SetProxyPreferences(proxy);
+
+            IWebDriver driver = new FirefoxDriver(profile);
+            return driver;
+        }
+
+        /// <summary>
+        /// Creates an ChromeDriver instance using the specified proxy settings.
+        /// </summary>
+        /// <param name="proxy">The WebDriver Proxy object containing the proxy settings.</param>
+        /// <returns>An ChromeDriver instance using the specified proxy settings</returns>
+        private static IWebDriver CreateChromeDriverWithProxy(Proxy proxy)
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.Proxy = proxy;
+
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+            return driver;
+        }
+
+        /// <summary>
+        /// Creates a PhantomJSDriver instance using the specified proxy settings.
+        /// </summary>
+        /// <param name="proxy">The WebDriver Proxy object containing the proxy settings.</param>
+        /// <returns>An InternetExplorerDriver instance using the specified proxy settings</returns>
+        private static IWebDriver CreatePhantomJSDriverWithProxy(Proxy proxy)
+        {
+            // This is an egregiously inconsistent API. Expect this to change
+            // so that an actual Proxy object can be passed in.
+            PhantomJSDriverService service = PhantomJSDriverService.CreateDefaultService();
+            service.ProxyType = "http";
+            service.Proxy = proxy.HttpProxy;
+
+            IWebDriver driver = new PhantomJSDriver(service);
+            return driver;
+        }
+    }
+}
